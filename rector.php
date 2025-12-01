f867ac3f144eac2664dc9e69e5088c004bd7d5e2
Nogrod <strikeomania@gmail.com>
2025-12-01 12:52:05 +0100

    latest

--- rector.php
+++ rector.php
@@ -2,33 +2,31 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\Class_\InlineConstructorDefaultToPropertyRector;
 use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodRector;
-use Rector\Doctrine\Set\DoctrineSetList;
 use Rector\Php80\Rector\Class_\ClassPropertyAssignToConstructorPromotionRector;
 use Rector\Config\RectorConfig;
 use Rector\Set\ValueObject\LevelSetList;
-use Rector\Symfony\Set\SymfonySetList;
 use Rector\Symfony\Symfony44\Rector\ClassMethod\ConsoleExecuteReturnIntRector;
 use Rector\Symfony\Symfony61\Rector\Class_\CommandPropertyToAttributeRector;
 
 return RectorConfig::configure()
     ->withPaths([
-                    __DIR__ . '/Command',
-                    __DIR__ . '/Console',
-                    __DIR__ . '/Controller',
-                    __DIR__ . '/Cron',
-                    __DIR__ . '/DependencyInjection',
-                    __DIR__ . '/Entity',
-                    __DIR__ . '/Event',
-                    __DIR__ . '/Exception',
-                    __DIR__ . '/Retry',
-                    __DIR__ . '/Twig',
-                    __DIR__ . '/View',
-                ])
+        __DIR__ . '/Command',
+        __DIR__ . '/Console',
+        __DIR__ . '/Controller',
+        __DIR__ . '/Cron',
+        __DIR__ . '/DependencyInjection',
+        __DIR__ . '/Entity',
+        __DIR__ . '/Event',
+        __DIR__ . '/Exception',
+        __DIR__ . '/Retry',
+        __DIR__ . '/Twig',
+        __DIR__ . '/View',
+    ])
     ->withSkip([
-                   RemoveUnusedPrivateMethodRector::class,
-               ])
+        __DIR__ . '/vendor',
+        RemoveUnusedPrivateMethodRector::class,
+    ])
     ->withAttributesSets()
     ->withPreparedSets(
         deadCode: true,
@@ -40,21 +38,18 @@ return RectorConfig::configure()
         symfonyCodeQuality: true,
         symfonyConfigs: true,
     )
+    ->withComposerBased(
+        twig: true,
+        doctrine: true,
+        phpunit: true,
+        symfony: true,
+    )
     ->withRules([
-                    InlineConstructorDefaultToPropertyRector::class,
-                    ClassPropertyAssignToConstructorPromotionRector::class,
-                    CommandPropertyToAttributeRector::class,
-                    ConsoleExecuteReturnIntRector::class
-                ])
+        ClassPropertyAssignToConstructorPromotionRector::class,
+        CommandPropertyToAttributeRector::class,
+        ConsoleExecuteReturnIntRector::class
+    ])
     ->withSets([
-                   LevelSetList::UP_TO_PHP_84,
-
-                   SymfonySetList::SYMFONY_72,
-                   SymfonySetList::SYMFONY_CONSTRUCTOR_INJECTION,
-
-                   DoctrineSetList::ANNOTATIONS_TO_ATTRIBUTES,
-                   DoctrineSetList::DOCTRINE_DBAL_40,
-                   DoctrineSetList::DOCTRINE_ORM_300,
-                   DoctrineSetList::DOCTRINE_CODE_QUALITY,
-               ])
+        LevelSetList::UP_TO_PHP_85,
+    ])
 ;
